690492629a8a45c59e4c419ecb70986aea909c2f
Unknown <lnguyen34@mail.csuchico.edu>
2017-04-25 19:27:02 -0700

    dsaf

--- gallery.js
+++ gallery.js
@@ -1,6 +1,12 @@
-
+var chico = ["Kwando Restaurant", "Peaking Restaurant", "Chico State Campus", "My Home", "Winco"];
+var current;
 
 function shuffleGallery()
 {
-
+  var nIndex
+  do {
+  nIndex  = Math.floor(Math.random()*chico.length);
+} while (nIndex == current);
+  current = nIndex;
+  console.log(chico[nIndex]);
 }
